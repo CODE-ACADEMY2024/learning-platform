f78e3f1d0084415ee33934434e95496143a07ba2
Viola <violatugumisirize990@gmail.com>
2024-07-15 17:40:13 +0300

    add form

---
+++ account/forms.py
@@ -0,0 +1,7 @@
+# your_app/forms.py
+from django import forms
+
+class MyForm(forms.Form):
+    name = forms.CharField(label='Your Name', max_length=100)
+    email = forms.EmailField(label='Your Email')
+    message = forms.CharField(widget=forms.Textarea, label='Your Message')
--- account/views.py
+++ account/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
-
 # Create your views here.
+# your_app/views.py
+from django.shortcuts import render, redirect
+from .forms import MyForm
+
+def my_form_view(request):
+    if request.method == 'POST':
+        form = MyForm(request.POST)
+        if form.is_valid():
+            # Process the form data
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+            # Do something with the data
+            return redirect('success')
+    else:
+        form = MyForm()
+
+    return render(request, 'your_app/form_template.html', {'form': form})
+    
+def success_view(request):
+    return render(request, 'your_app/success.html')
